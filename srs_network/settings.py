bc78ccbf5fe526bbc5a0d5da50ed581593c2b3bd
Jakob Tolstrup <jakob.l.tolstrup@gmail.com>
2020-08-23 09:19:15 +0200

    changed to django-crontab

--- srs_network/settings.py
+++ srs_network/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'django_crontab',
+    'django-crontab',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
