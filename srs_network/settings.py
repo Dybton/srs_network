efb66951c8f38c7a85d6598da04f6644c7e8c2eb
Jakob Tolstrup <jakob.l.tolstrup@gmail.com>
2020-08-24 16:13:34 +0200

    added django-heroku

--- srs_network/settings.py
+++ srs_network/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 """
 
 import os
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -182,3 +183,5 @@ CRONJOBS = [
     ('0 0 * * *',
      'spaced_repitition.cron.decrement_days_till_study')
 ]
+
+django_heroku.settings(locals())
